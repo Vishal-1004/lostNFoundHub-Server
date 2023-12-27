d70b65b7c139fc319e7c8ecd818217477cf79f63
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2023-12-28 04:18:15 +0530

    First commit

---
+++ helper/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dcmqniwwc",
+  api_key: "348472516959522",
+  api_secret: "ftiefl_ctfq0yUwNmVCLybIr0KY",
+});
+
+module.exports = cloudinary;
